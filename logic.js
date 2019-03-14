c270f0084c05218365d9eeed5adad297804bffa4
Jessica Jones <47366649+vguercio10@users.noreply.github.com>
2019-03-13 21:02:12 -0400

    completed moment conversion

--- logic.js
+++ logic.js
@@ -6,31 +6,76 @@ var config = {
     projectId: "train-times-92dc1",
     storageBucket: "train-times-92dc1.appspot.com",
     messagingSenderId: "740815935111"
-  };
-  firebase.initializeApp(config);
+};
 
-// Assigned the reference to the database to a variable
+firebase.initializeApp(config);
+
 
 var database = firebase.database();
 
-var trainName = " ";
-var trainDestination = " ";
-var trainFrequency = 0;
-var nextArrival = 0;
-var minutesAway = 0;
-
-$("#addTrain").on("click", function(event){
+// Button for adding trains
+$("#addTrain").on("click", function (event) {
     event.preventDefault();
-console.log("button clicked")
+
+    // Grabs user input
+    var tName = $("#trainName").val().trim();
+    var tDestination = $("#trainDestination").val().trim();
+    var firstTTime = $("#firstTrainTime").val().trim();
+    var tfrequency = $("#trainFrequency").val().trim();
+
+    // Creates local "temporary" object for holding train data
+    var newTrain = {
+        name: tName,
+        destination: tDestination,
+        firstTrain: firstTTime,
+        frequency: tfrequency,
+    };
+
+    // Uploads new train to database
+    database.ref().push(newTrain);
+
+    // Logs to console
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.firstTrain);
+    console.log(newTrain.frequency);
 
 
-employee = $("#employee-name-input").val().trim();
-    role = $("#role-input").val().trim();
-    start = $("#start-input").val().trim();
-    rate = $("#rate-input").val().trim();
-
-    console.log(employee);
-    console.log(role);
-    console.log(start);
-    console.log(rate);
+    // Clears all of the text-boxes
+    $("#trainName").val("");
+    $("#trainDestination").val("");
+    $("#firstTrainTime").val("");
+    $("#trainFrequency").val("");
 });
+// Moment
+database.ref().on("child_added", function (childSnapShot) {
+    console.log(childSnapShot.val());
+    var trainData = childSnapShot.val();
+    var convertedFirstTrain = moment(trainData.firstTrain, "HH:mm");
+    console.log(convertedFirstTrain);
+    var differenceCurrentTimeTrainTime = moment().diff(moment(convertedFirstTrain), "minutes");
+    console.log(differenceCurrentTimeTrainTime);
+    var timeRemaining = differenceCurrentTimeTrainTime % trainData.frequency;
+    var minutesAway = trainData.frequency - timeRemaining;
+    var nextArrival = moment().add(minutesAway, "minutes");
+    nextArrival = moment(nextArrival).format("HH:mm");
+    var name = childSnapShot.val().name;
+    var destination = childSnapShot.val().destination;
+    var firstTrain = childSnapShot.val().firstTrain;
+    var frequency = childSnapShot.val().frequency;
+// Created variable to hold data and append to html
+    var newRow = $("<tr>").append(
+        $("<td>").text(name),
+        $("<td>").text(destination),
+        $("<td>").text(frequency),
+        $("<td>").text(minutesAway),
+        $("<td>").text(nextArrival),
+    );
+    $("tbody").append(newRow);
+});
+
+
+
+
+
+
